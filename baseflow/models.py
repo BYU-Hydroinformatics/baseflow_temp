36be399cba53ab4d60fa9ac9a4de6ed3b6856e27
xueyil9 <xueyi@xueyideair.lan>
2024-04-29 11:17:42 -0600

    added HydRun

--- baseflow/models.py
+++ baseflow/models.py
@@ -166,6 +166,7 @@ def chapman_maxwell(streamflow_list, k):
 
         return baseflow_list
     
+    
 def HydRun(streamflow_list, k, passes):
     """
     Separates baseflow from a streamflow hydrograph using a digital filter method.
